ec3e4d6432d77b9bc6c8f2c2462342af33ceb3f8
Mike Clancy <mclancy96@gmail.com>
2023-11-12 11:42:54 -0500

    everything setup. need to work on logic

--- app.js
+++ app.js
@@ -4,16 +4,28 @@
 
 import express from 'express';
 import 'dotenv/config';
+import { configDotenv } from 'dotenv';
 
 var app = express();
+configDotenv();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use(express.static('/public'));
+app.use(express.urlencoded({ extended: true }));
 
+app.post('/product', (req, res) => {
+  if (!req.body || !req.body.dpci) {
+    res.status(400).send('Missing information. Please include at least a dpci.')
+    return;
+  }
+  res.render('result', { dpci: req.body.dpci })
+})
 app.get('/', (req, res) => { res.render('index') });
 
+
+
 app.listen(process.env.PORT, () => {
-  console.log("Fulfillment Manager has started...");
+  console.log("Product help bot has started on port", process.env.PORT);
 });

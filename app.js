5f25d5d0a748060c66281c6bc562492cfaf61198
Mike Clancy <mclancy96@gmail.com>
2023-11-14 10:06:57 -0500

    pushing to update

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
  */
 
 import express from 'express';
-import 'dotenv/config';
 import { configDotenv } from 'dotenv';
+import { CLASS_NAMES } from './constants.js';
 
 var app = express();
 configDotenv();
@@ -15,17 +15,21 @@ app.set('view engine', 'ejs');
 app.use(express.static('/public'));
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => { res.render('index') });
+
 app.post('/product', (req, res) => {
   if (!req.body || !req.body.dpci) {
     res.status(400).send('Missing information. Please include at least a dpci.')
     return;
   }
-  res.render('result', { dpci: req.body.dpci })
+  const classN = getClassN(req.body.dpci);
+  res.render('result', { dpci: req.body.dpci, department: req.body.dpci.substring(0, 3), classID: req.body.dpci.substring(0, 6), classN: classN, delivery: req.body.delivery, sold: req.body.delivery })
 })
-app.get('/', (req, res) => { res.render('index') });
 
-
-
+const getClassN = (dpci) => {
+  const classID = dpci.substring(0, 6);
+  return CLASS_NAMES[classID];
+}
 app.listen(process.env.PORT, () => {
   console.log("Product help bot has started on port", process.env.PORT);
 });
